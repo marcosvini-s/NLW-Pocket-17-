496b613b5c925f3b4dd112235610c04a624bfa13
Marcos Vinicius <marcos.sampaiopeixoto@hotmail.com>
2024-09-13 11:05:05 -0300

    fundamentos 001

---
+++ index.js
@@ -0,0 +1,14 @@
+let meta = {
+    value: 'ler um livro por mês',
+    checked: false,
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false,
+    }
+]
+
+console.log(metas[1].value)
