6eb15fdebb1c5d129c06038ac8bdd99d8674c7cf
Marcos Vinicius <marcos.sampaiopeixoto@hotmail.com>
2024-09-13 15:37:06 -0300

    menu da aplicação

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const start = () => {
+const{ select } = require('@inquirer/prompts')
+
+const start = async() => {
     while(true){
-        let opcao ="sair"
-        switch(opcao){
+        
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        switch(opcao) {
             case "cadastrar":
                 console.log("vamos cadastrar")
                 break
             case "listar":
-                console.log("vamos cadastrar")
+                console.log("vamos listar")
                 break
             case "sair":
+                console.log("Até a próxima")
                 return
         }
     }
